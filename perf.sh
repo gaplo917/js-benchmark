9d2d182b2dd97e0c8b5d547d7d57c85d2d104d54
Gary Lo <logap@me.com>
2020-08-29 16:30:21 +0800

    pull out to perf.sh

---
+++ perf.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+ARR_SIZE=1 yarn start;
+ARR_SIZE=10 yarn start;
+ARR_SIZE=100 yarn start;
+ARR_SIZE=1000 yarn start;
+ARR_SIZE=10000 yarn start;
+ARR_SIZE=100000 yarn start;
+ARR_SIZE=1000000 yarn start;
